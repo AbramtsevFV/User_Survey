5ca13d949e214daeada538fbb1ba7558e063384f
AbramtsevFV <75078217+AbramtsevFV@users.noreply.github.com>
2021-07-05 09:46:04 +0300

    fix

--- surveys/urls.py
+++ surveys/urls.py
@@ -8,8 +8,9 @@ urlpatterns = [
                 path('auth/', auth_view, name='auth'),
                 # Для Админов
                 # Опрос
-
+                path('survey_create/', survey_create, name='survey_create'),
                 path('survey_update_del/<int:survey_id>', survey_update_del, name='survey_update_del'),
+                path('survey_list/', survey_list, name='survey_list'),
                 # Вопрос
                 path('question_create/', question_create, name='question_create'),
                 path('question_update_del/<int:question_id>', question_update_del, name='question_update_del'),
