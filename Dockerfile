936018d642f723eb4c4686cfbfd895966c24df35
AbramtsevFV <75078217+AbramtsevFV@users.noreply.github.com>
2021-07-12 16:39:05 +0300

    fix

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 # pull official base image
-FROM python:3.8.3-alpine
+FROM python:3.8
 # set work directory
 RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
-# set environment variables
+# не добовляются файла .pyc
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
 # install dependencies
--- User_Survey/settings.py
+++ User_Survey/settings.py
@@ -75,14 +75,25 @@ WSGI_APPLICATION = 'User_Survey.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# Настройки для подключения к БД sqlite3
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': str(os.path.join(BASE_DIR, "db.sqlite3")),
+#     }
+# }
+
+# Настройки для подключения к БД Postgres
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3")),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'postgresql_db',
+        'PORT': 5432,
     }
-}
+ }
 
 
 # Password validation
@@ -109,13 +120,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'Europe/Moscow'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
